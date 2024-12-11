67d9c30164ae7bf3a428802e938272514ee96c21
Yash <cse210001011@iiti.ac.in>
2024-12-11 23:52:59 +0530

    getapi

--- src/app.js
+++ src/app.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const app = express();
 
+// app.use("/",(req,res)=>{
+//     res.send("Hello , Dashboard here ");
+// });
+
+app.get("/user",(req,res)=>{
+    res.send({firstname:"yash", lastname:"chandramore"});
+})
+
+app.post("/user",(req,res)=>{
+    res.send("data sent successfully");
+})
+
 app.use("/test",(req,res)=>{
     res.send("test from the server ");
 });
@@ -8,6 +20,8 @@ app.use("/test",(req,res)=>{
 app.use("/hello",(req,res)=>{
     res.send("Hello jii from the server ");
 });
+
+
 
 app.listen(3000,()=>{
     console.log("Serveris listening at 3000 ");
